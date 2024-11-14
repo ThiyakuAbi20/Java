a79349075fc51359eaed079230dcea4c28368697
ThiyakuAbi20 <abiramythiyakesan280@gmail.com>
2024-11-14 15:36:07 +0530

    #3.3, route added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+
+const isAuthenticated = () => {
+  return sessionStorage.getItem("isAuthenticated") === "true"; 
+};
+
+const PrivateRoute = ({ element, ...rest }) => {
+  return isAuthenticated() ? element : <Navigate to="/login" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<PrivateRoute element={<HomePage />} />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+

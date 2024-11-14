2c557fda064cfa26adea04a483a51562794271aa
ThiyakuAbi20 <abiramythiyakesan280@gmail.com>
2024-11-14 16:52:50 +0530

    small changes

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -13,8 +13,12 @@ function HomePage() {
   return (
     <div className="home-page">
       <h2>Welcome to the Home Page!</h2>
-      <p>Welcome to the Attendance System! Our platform streamlines attendance tracking, allowing for efficient and accurate record-keeping. Easily manage student or employee attendance, track time and presence, and generate reports with just a few clicks. Designed for convenience and reliability, our system helps organizations save time and improve productivity by providing real-time attendance insights. Explore a smarter way to handle attendance today!</p>
-      <button onClick={handleLogout}>Logout</button>
+      <div className="content-box">
+        <p>
+          Welcome to the Attendance System! Our platform streamlines attendance tracking, allowing for efficient and accurate record-keeping. Easily manage student or employee attendance, track time and presence, and generate reports with just a few clicks. Designed for convenience and reliability, our system helps organizations save time and improve productivity by providing real-time attendance insights. Explore a smarter way to handle attendance today!
+        </p>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
     </div>
   );
 }

7d64a5f147ff16895dae1e666a01a6fbdf478583
ThiyakuAbi20 <abiramythiyakesan280@gmail.com>
2024-11-14 15:38:23 +0530

    #4, fetch login and home page

---
+++ client/src/services/authServices.js
@@ -0,0 +1,23 @@
+export const login = async (username, password) => {
+    try {
+      const response = await fetch(`${API_URL}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+  
+      if (response.ok) {
+        sessionStorage.setItem("isAuthenticated", "true");
+        return true;
+      } else {
+        const errorData = await response.json();
+        console.error('Login failed:', errorData);
+        return false;
+      }
+    } catch (error) {
+      console.error('Network error or invalid response:', error);
+      return false;
+    }
+};  
